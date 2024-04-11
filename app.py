fcefa60dacef8d39ef11a55f6418e047c5b5cb22
Ivaylo Keremidarski <12044844+keremidarski@users.noreply.github.com>
2024-04-11 22:02:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect
 from sock_puppet import SockPuppet
 
 app = Flask(__name__)
@@ -9,10 +9,27 @@ def index():
 
 @app.route('/generate', methods=['POST'])
 def generate():
-    gender = request.form.get('gender', 'random')
-    puppet = SockPuppet(gender)
+    puppet = SockPuppet("random")
 
-    return render_template('index.html', puppet=puppet)
+    puppet_data = {
+        "photo": puppet.photo,
+        "cyrillic_first_name": puppet.cyrillic_first_name,
+        "cyrillic_family_name": puppet.cyrillic_family_name,
+        "gender": puppet.gender,
+        "dob": puppet.bg_date_of_birth,
+        "age": puppet.age,
+        "star_sign": puppet.star_sign,
+        "egn": puppet.egn,
+        "address": puppet.address,
+        "phone_number": puppet.phone_number,
+        "credit_card": puppet._credit_card,
+        "email": puppet.email,
+        "username": puppet.username,
+        "password": puppet.password,
+        "website_domain": puppet.website_domain,
+    }
+
+    return jsonify(puppet_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
